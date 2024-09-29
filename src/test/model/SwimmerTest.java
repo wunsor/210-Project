1ba7ee030ebb8dacc093ee1204038711f725c4ba
Winsor Zhu <wzhu22@student.ubc.ca>
2024-09-28 19:01:13 -0700

    Removed/renamed/added classes for relevance

---
+++ src/test/model/SwimmerTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SwimmerTest {
+    
+    @BeforeEach
+    void runBefore() {
+
+    }
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
