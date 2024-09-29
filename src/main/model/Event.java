8f4cc00a0ae43b0fd7325d0efe06acca7d98b590
Winsor Zhu <wzhu22@student.ubc.ca>
2024-09-28 19:01:42 -0700

    Began specification for Event class

---
+++ src/main/model/Event.java
@@ -0,0 +1,22 @@
+package model;
+
+// Represents an Event which a swimmer might have, includes the name of the event and a time
+public class Event {
+    private String eventName; // name of the event, ie. fly
+    private double eventTime; // best time of the swimmer in that event
+
+    // REQUIRES: event must be one of fly, back, breast, or free
+    public Event(String name, double time) {
+
+    }
+
+    //setters and getters
+    public String getEventName() {
+        return null;
+    }
+
+    public double getEventTime() {
+        return 0;
+    }
+
+}
