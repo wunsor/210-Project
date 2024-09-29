e38f050209492039f6f3a099168fb9b258b9a408
Winsor Zhu <wzhu22@student.ubc.ca>
2024-09-28 19:02:20 -0700

    Began specification for ParticipatingSwimmers class

---
+++ src/main/model/ParticipatingSwimmers.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.util.List;
+
+// Represents a list of participating swimmers at a swim meet
+public class ParticipatingSwimmers {
+    private List<Swimmer> swimmers; //a list of female swimmers
+
+    public ParticipatingSwimmers() {
+
+    }
+
+    public void organizeIntoHeats() {
+
+    }
+
+    public void lookupSwimmer() {
+
+    }
+
+}
