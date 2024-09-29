2900c5ed2b3b7890b8b0bd181ada82f3be17dd03
Winsor Zhu <wzhu22@student.ubc.ca>
2024-09-28 19:02:33 -0700

    Began specification for Swimmer class

---
+++ src/main/model/Swimmer.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.List;
+
+// Represents a swimmer with, a name, Events, and gender 
+public class Swimmer {
+    String swimmerName; //the name of the swimmer
+    List<Event> events; // list of the swimmers events
+
+    public Swimmer(String name, Event events) {
+
+    }
+
+
+    //getters and setters
+
+    public String getSwimmerName() {
+        return null;
+    }
+
+    public List<Event> getSwimmerEvents() {
+        return null;
+    }
+}
