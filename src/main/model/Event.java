c82f589c15506614499b548305bbce59879f96f9
Winsor Zhu <wzhu22@student.ubc.ca>
2024-10-02 09:32:55 -0700

    Specification for Event class

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -3,9 +3,9 @@ package model;
 // Represents an Event which a swimmer might have, includes the name of the event and a time
 public class Event {
     private String eventName; // name of the event, ie. fly
-    private double eventTime; // best time of the swimmer in that event
+    private double eventTime; // best time of the swimmer in that event in seconds
 
-    // REQUIRES: event must be one of fly, back, breast, or free
+    // REQUIRES: name must be one of fly, back, breast, or free. time is a time in seconds
     public Event(String name, double time) {
 
     }
