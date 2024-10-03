87fdbe4060dcd98c0283f2bf33398c8cc02e15c5
Winsor Zhu <handu.winsor@gmail.com>
2024-10-03 13:41:43 -0700

    Completed implementation for ParticipatingSwimmers class

--- src/main/model/ParticipatingSwimmers.java
+++ src/main/model/ParticipatingSwimmers.java
@@ -8,7 +8,7 @@ public class ParticipatingSwimmers {
 
     //EFFECT: a list of all participating swimmer which is empty when initialized
     public ParticipatingSwimmers() {
-
+        this.swimmers = new ArrayList<>();
     }
 
     //REQUIRES: event must be one of fly, back, breast, or free
@@ -19,28 +19,44 @@ public class ParticipatingSwimmers {
 
     }
 
-    //EFFECTS: takes a swimmer's name, returns their events as a string 
+    //REQUIRES: name must be one the swimmers names already in the list
+    //EFFECTS: takes a swimmer's name, returns their events as a list of string 
     public ArrayList<String> lookupSwimmersEvents(String name) {
-        return null;
+        ArrayList<String> eventsStringList;
+        eventsStringList = new ArrayList<>();
+        for (Swimmer s : swimmers) {
+            if (s.getSwimmerName() == name) {
+                for (Event e : s.getSwimmerEvents()) {
+                    eventsStringList.add(e.getEventName());
+                }
+                return eventsStringList;
+            }
+        }
+        return eventsStringList;
 
     }
 
     //MODIFIES: this
     //EFFECTS: takes a swimmer and adds them to the list of participating swimmers
     public void addSwimmer(Swimmer swimmer) {
-
+        this.swimmers.add(swimmer);
     }
 
+    //REQUIRES: name matches one of the names in the list of participating swimmers
     //MODIFIES: this
     //EFFECTS: takes a swimmers name as a string and removes them from the list of participating swimmers
     public void removeSwimmer(String name) {
-
+        for (int i = 0; i < swimmers.size(); i++) {
+            if (swimmers.get(i).getSwimmerName() == name) {
+                this.swimmers.remove(swimmers.get(i));
+            }
+        }
     }
 
     // setters and getters
 
-    public ArrayList<Swimmer> getParticipatingSwimmers(){
-        return null;
+    public ArrayList<Swimmer> getParticipatingSwimmers() {
+        return this.swimmers;
     }
 
 
