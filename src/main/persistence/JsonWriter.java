8e2374fe5231993a856d71748b41f6f54f620381
Winsor Zhu <wzhu22@student.ubc.ca>
2024-10-21 12:50:00 -0700

    Specified JsonWriter class

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,34 @@
 package persistence;
 
+import java.io.FileNotFoundException;
+
+import model.ParticipatingSwimmers;
+
+// Represents a writer that writes JSON representation of ParticipatingSwimmers to file
 public class JsonWriter {
+
+    //EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: opens writer, thrwos FileNotFoundException if destination file
+    //         cannot be opened for writing
+    public void open() throws FileNotFoundException {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: writes JSON representation of ParticipatingSwimmers to file
+    public void write(ParticipatingSwimmers ps) {
+
+    }  
+
+    //MODIFIES: this
+    //EFFECTS: closes writer
+    public void close() {
+
+    }
 
 }
