9f7dc39a9608b0fae91ca41555d75c71aa2dda2c
Winsor Zhu <wzhu22@student.ubc.ca>
2024-10-21 12:45:26 -0700

    Specified JsonReader class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,5 +1,21 @@
 package persistence;
 
+import java.io.IOException;
+
+import model.ParticipatingSwimmers;
+
+// Represents a reader that reads ParticipatingSwimmers from JSON data stored in file
 public class JsonReader {
+
+    //EFFECTS: constrcuts reader to read from source file
+    public JsonReader(String source) {
+
+    }
+
+    //EFFECTS: reads ParticipatingSwimmers from file and return it,
+    //         throws IOException if an error occurs reading data from file
+    public ParticipatingSwimmers read() throws IOException {
+        return null;
+    }
 
 }
